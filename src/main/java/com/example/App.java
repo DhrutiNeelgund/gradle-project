e42f193d81654a1badec1fd2121100efc3670d44
DhrutiNeelgund <dhrutidvg2005@gmail.com>
2026-03-27 10:47:46 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from Gradle!");
+        System.out.println("Hello from Gradle project!");
     }
 }
